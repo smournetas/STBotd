598f2472d4f132fa7c565dc8c00427f411be5f97
Sebastien Mournetas <sebastien.mournetas@gmail.com>
2012-03-15 20:09:09 +0100

    - Add mp4 file support

--- STBotd.py
+++ STBotd.py
@@ -18,7 +18,7 @@ from datetime import datetime
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/lib/')
 import feedparser
 
-PRG_VERSION = '0.6.2 EatMyShorts'
+PRG_VERSION = '0.6.1 EatMyShorts'
 PRG_NAME, _ = os.path.splitext(os.path.basename(__file__))
 WRK_DIR = os.path.dirname(os.path.abspath(__file__))
 TMP_DIR = WRK_DIR + '/tmp'
@@ -447,9 +447,7 @@ def getSuitableFilesListFromZip(file, ep, file_ext, prefix='', indent=''):
 		else:
 			full_name = prefix + '/' + unicode(info.filename, 'iso-8859-1')
 			if not re.match('.+[\.\- ](' + config['process.exclude'] + ')[\.\- ]', full_name, re.IGNORECASE) \
-			and ( re.match('.+[\.\-\ ](' + release + ').*(' + file_ext + ')$', full_name, re.IGNORECASE) \
-				or re.match('.+(' + release + ')[\.\-\ ].*(' + file_ext + ')$', full_name, re.IGNORECASE) \
-				or re.match('.+[\.\-\ ](' + alt_release + '|' + alt_release2 + ')[\.\-\ ].*(' + file_ext + ')$', full_name, re.IGNORECASE) ):
+			and re.match('.+[\.\-\ ](' + release + '|' + alt_release + '|' + alt_release2 + ')[\.\-\ ].*(' + file_ext + ')$', full_name, re.IGNORECASE):
 				log.info(indent + '[X] File ' + os.path.relpath(full_name, TMP_DIR))
 				matches.append(full_name)
 				zip.extract(info, prefix)
@@ -489,7 +487,7 @@ def process(url, backlog):
 				if os.path.isdir(path):
 					log.info('Scan in progress : ' + path)
 					for file in os.listdir(path):
-						if not file.startswith('.') and ( file.endswith('.avi') or file.endswith('.mkv')):
+						if not file.startswith('.') and ( file.endswith('.avi') or file.endswith('.mkv') or file.endswith('.mp4') ):
 							ep_attr = episodeAttributes(file, shows[show])
 
 							if ( ep_attr['number'] == ep_number or ep_attr['alt_number'] == ep_number ) and ep_attr['season'] == season:
